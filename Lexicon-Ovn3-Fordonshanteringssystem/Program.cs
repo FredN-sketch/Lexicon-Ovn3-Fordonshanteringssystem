892046d06ce8a1ba4e847a6b3cf55e12dead8362
Fredrik Norgren <fredrik_norgren@hotmail.com>
2025-04-11 10:00:33 +0200

    Moving code to methods in Menu, cleaning up a bit

--- Lexicon-Ovn3-Fordonshanteringssystem/Program.cs
+++ Lexicon-Ovn3-Fordonshanteringssystem/Program.cs
@@ -24,47 +24,12 @@ internal class Program
         {
             MenuHelper.PrintMenu();
             showMenu = MainMenu();
-        }
-        return;
-        //   Vehicle bil1 = new Vehicle("Volvo", "240", 1980, 2.0);
-        //   Vehicle bil2 = new Vehicle("V", "", 1789, -2.0);
-        //   //Vehicle bil2 = new Vehicle("Volvo", "245", 2025, 3);
-        try
-        {
-            Vehicle vehicle1 = VehicleHandler.AddVehicle();
-            Console.WriteLine(vehicle1);
-            Console.ReadLine();
-            //VehicleHandler.SetBrand(vehicle1);
-            //VehicleHandler.SetModel(vehicle1);
-            //VehicleHandler.SetYear(vehicle1);
-            //VehicleHandler.SetWeight(vehicle1);
-            if (vehicle1.Type == "Car") { }
-            Console.WriteLine(vehicle1);
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine(ex.Message);
-        }
-
-        //SystemError bromsfel = new BrakeFailureError();
-        //SystemError motorfel = new EngineFailureError();
-        //SystemError vaxelfel = new TransmissionError();
-        //var systemfel = new List<SystemError> { bromsfel, motorfel, vaxelfel };
-        //foreach (SystemError error in systemfel) 
-        //{
-        //    Console.WriteLine(error.ErrorMessage());
-        //}
-
-
-        
-            
-       Console.ReadLine();
+        }    
     }
 
     //Console.WriteLine("Fordonshantering");
     //Console.WriteLine("================");
     //Console.WriteLine("1. Lägg till fordon manuellt");
-    //Console.WriteLine("1b. Lägg till fordon automatiskt");
     //Console.WriteLine("2. Visa fordon");
     //Console.WriteLine("3. Skapa och visa fellista");
     //Console.WriteLine("4. Visa fellista");            
@@ -76,61 +41,30 @@ internal class Program
             string userInput = Console.ReadLine();
             switch (userInput)
             {
-                case "0":                                       // 0. Avsluta
-                    {
-                        return false;
-                    }
                 case "1":                                       // 1. Lägg till fordon manuellt
                     {
                         MenuAddVehicle();
                         return true;
                     }
-                case "1b":                                       // 1b. Lägg till fordon automatiskt
-                    {
-                        SeedData();
-                        return true;
-                    }
+                //case "1b":                                       // 1b. Lägg till fordon automatiskt
+                //    {
+                //        SeedData();
+                //        return true;
+                //    }
                 case "2":                                       // 2. Visa fordon
                     {
-                        List<Vehicle> vehicles = SeedData();
-                        foreach (Vehicle vehicle in vehicles)
-                        {
-                            vehicle.Stats();
-                            //Console.WriteLine(vehicle);
-                            vehicle.StartEngine();
-                            if (vehicle is ICleanable)
-                            {
-                                if (vehicle is Car)
-                                    ((Car)vehicle).Clean();     //cast för att kunna anropa Clean i Car
-                                else if (vehicle is Truck)
-                                    ((Truck)vehicle).Clean();   //cast för att kunna anropa Clean i Truck
-                            }
-
-                        }
-                        Console.ReadLine();
+                        MenuListVehicles();                        
                         return true;
                     }
                 case "3":                                       // 3. Fellista. Skapar och skriver ut en lista med 10 slumpmässiga fel
                     {
-                        List<SystemError> systemfel = new List<SystemError>(); 
-                        for (int i = 0; i < 10; i++)
-                        {
-                            int slumpfel = Random.Shared.Next(1, 3 + 1);
-                            if (slumpfel == 1)
-                                systemfel.Add(new BrakeFailureError());
-                            else if (slumpfel == 2)
-                                systemfel.Add(new EngineFailureError());
-                            else
-                                systemfel.Add(new TransmissionError());
-                        }
-                        foreach (SystemError error in systemfel)
-                        {
-                            Console.WriteLine(error.ErrorMessage());
-                        }
-                        Console.ReadLine();
+                        MenuListErrors();
                         return true;
                     }               
-
+                case "0":                                       // 0. Avsluta
+                    {
+                        return false;
+                    }
                 default:
                     {                                           // om användaren matat in något annat än menyvalen
                         Console.WriteLine("Ogiltigt val");
@@ -223,6 +157,46 @@ internal class Program
 
     }
 
+    private static void MenuListVehicles()
+    {
+        List<Vehicle> vehicles = SeedData();
+        foreach (Vehicle vehicle in vehicles)
+        {
+            vehicle.Stats();
+            //Console.WriteLine(vehicle);
+            vehicle.StartEngine();
+            if (vehicle is ICleanable)
+            {
+                if (vehicle is Car)
+                    ((Car)vehicle).Clean();     //cast för att kunna anropa Clean i Car
+                else if (vehicle is Truck)
+                    ((Truck)vehicle).Clean();   //cast för att kunna anropa Clean i Truck
+            }
+
+        }
+        Console.ReadLine();
+
+    }
+    private static void MenuListErrors()
+    {
+        List<SystemError> systemfel = new List<SystemError>();
+        for (int i = 0; i < 10; i++)
+        {
+            int slumpfel = Random.Shared.Next(1, 3 + 1);
+            if (slumpfel == 1)
+                systemfel.Add(new BrakeFailureError());
+            else if (slumpfel == 2)
+                systemfel.Add(new EngineFailureError());
+            else
+                systemfel.Add(new TransmissionError());
+        }
+        foreach (SystemError error in systemfel)
+        {
+            Console.WriteLine(error.ErrorMessage());
+        }
+        Console.ReadLine();
+    }
+
     private static List<Vehicle> SeedData()
     {
         Car car1 = new Car("Volvo", "245", 1985, 2, 5);
@@ -231,7 +205,7 @@ internal class Program
         ElectricScooter scoot = new ElectricScooter("Ola", "S1", 2022, 0.1, 3);
         Motorcycle mc = new Motorcycle("Kawasaki", "Z650", 2022, 0.2, false);
         List<Vehicle> vehicles = new List<Vehicle>() { car1, car2, truck, scoot, mc };
-        //List<Motorcycle> mcs = new List<Motorcycle> { mc, car1 };
+        //List<Motorcycle> mcs = new List<Motorcycle> { mc, car1 }; // kan ej lägga till car1 i List<Motorcycle>
         
         return vehicles;
     }
